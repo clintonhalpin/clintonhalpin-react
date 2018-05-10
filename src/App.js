0caeab6843b12fc80fb8764308258bcba95c6e1d
Clinton Halpin <chalpin@ex.io>
2018-05-09 20:01:43 -0400

    Add some crypto stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
-import qc from './qc.jpg';
-import hitch from './hitch.jpg';
-// import sf from './sf.jpg';
-// import cmu from './cheermeup.jpg';
-// import ssa from './ssa.jpg';
+import React, { Component } from "react";
+import qc from "./qc.jpg";
+import hitch from "./hitch.jpg";
+import crypto from "./crypto.jpg";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
         <div className="bg-white">
           <div className="container mx-auto">
             <div className="table col-12 px2 sm-px0">
               <div className="table-cell">
-                <a className="bold black inline-block no-underline" href="/">Clinton Halpin</a>
+                <a className="bold black inline-block no-underline" href="/">
+                  Clinton Halpin
+                </a>
               </div>
               <div className="table-cell right-align">
                 <ul className="list-reset m0">
-                  <li className="inline-block"><a className="black inline-block p2 sm-p3" href="https://github.com/clintonhalpin">Github</a></li>
-                  <li className="inline-block"><a className="black inline-block" href="https://twitter.com/clintonhalpin">Twitter</a></li>
+                  <li className="inline-block">
+                    <a
+                      className="black inline-block p2 sm-p3"
+                      href="https://github.com/clintonhalpin"
+                    >
+                      Github
+                    </a>
+                  </li>
+                  <li className="inline-block">
+                    <a
+                      className="black inline-block"
+                      href="https://twitter.com/clintonhalpin"
+                    >
+                      Twitter
+                    </a>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -28,34 +41,77 @@ class App extends Component {
         </div>
 
         <div className="container mx-auto px2">
+          <h1 className="jumbo tight">
+            cryptocurrency tech lead, product, ux &amp; ui background.
+          </h1>
 
-          <h1 className="jumbo tight">technology lead, product, ux &amp; ui background.</h1>
+          <h2 className="mb0">
+            <span className="red">[stealth] Crypto Exchange</span>
+            <span className="h5 ml2">Present</span>
+          </h2>
+          <hr />
+          <img src={crypto} className="fit" alt="Quote Catalog" />
+          <p className="mt0">
+            Currently I am working on building a new cryptocurrency exchange.
+            Stay tuned for updates!
+          </p>
 
-          <h2 className="mb0"><a href="https://quotecatalog.com">Quote Catalog</a><span className="h5 ml2">Present</span></h2>
+          <ul className="fancy-list list-reset">
+            <li>Lead Frontend Developer, UX Designer and branding</li>
+            <li>Infrastructure and Devops leader</li>
+            <li>Building out our team! Please reach out!</li>
+          </ul>
+
+          <h2 className="mb0">
+            <a href="https://quotecatalog.com">Quote Catalog</a>
+            <span className="h5 ml2">2013-2017</span>
+          </h2>
           <hr />
           <img src={qc} className="fit" alt="Quote Catalog" />
-          <p className="mt0">A platform dedicated to becoming the Quote Engine of the internet.</p>
+          <p className="mt0">
+            A platform dedicated to becoming the Quote Engine of the internet.
+          </p>
 
           <ul className="fancy-list list-reset">
             <li>Chatbots and automation</li>
             <li>Search engines</li>
-            <li>Ranking Algorithms, Machine Learning, Natrual Language Processing</li>
+            <li>
+              Ranking Algorithms, Machine Learning, Natrual Language Processing
+            </li>
           </ul>
-          
-          <h2 className="m0"><a href="https://salesforce.com">Salesforce</a><span className="h5 ml2">2013-2016</span></h2>
+
+          <h2 className="m0">
+            <a href="https://salesforce.com">Salesforce</a>
+            <span className="h5 ml2">2013-2016</span>
+          </h2>
           <hr />
-          <p className="mt0">For three years I played a vital role on the UX team for the Salesforce Marketing Cloud.</p>
+          <p className="mt0">
+            For three years I played a vital role on the UX team for the
+            Salesforce Marketing Cloud.
+          </p>
 
           <ul className="fancy-list list-reset">
             <li>UX Lead on 2 Mobile applications ( iOS / Android )</li>
-            <li>Developed Style Guides and Design tooling systems that shipped into production</li>
-            <li>UX Lead on Social Account management ( many many other Admin features )</li>
+            <li>
+              Developed Style Guides and Design tooling systems that shipped
+              into production
+            </li>
+            <li>
+              UX Lead on Social Account management ( many many other Admin
+              features )
+            </li>
           </ul>
-          
-          <h2 className="m0"><a href="https://thehitch.com">the Hitch</a><span className="h5 ml2">2015-2016</span></h2>
+
+          <h2 className="m0">
+            <a href="https://thehitch.com">the Hitch</a>
+            <span className="h5 ml2">2015-2016</span>
+          </h2>
           <hr />
-          <img src={ hitch } className="fit" alt="The Hitch" />
-          <p className="mt0">A platform for searching and discovering new wedding venues nearby with upfront pricing and amenity listings.</p>
+          <img src={hitch} className="fit" alt="The Hitch" />
+          <p className="mt0">
+            A platform for searching and discovering new wedding venues nearby
+            with upfront pricing and amenity listings.
+          </p>
 
           <ul className="fancy-list list-reset">
             <li>Front-End Development</li>
@@ -64,115 +120,143 @@ class App extends Component {
 
           <h2 className="dark-black py2 m0">Even More</h2>
 
-          <h2 className="m0"><a href="http://sawsomeart.com">Saw Some Art</a></h2>
+          <h2 className="m0">
+            <a href="http://sawsomeart.com">Saw Some Art</a>
+          </h2>
           <hr />
           <p className="mt0">My art instagram account and API</p>
 
-          <h2 className="m0"><a href="https://docs.google.com/presentation/d/1AGFl4n_bpdYJLvjceJzYUMSJ1dxMYqbqkH1WG69ALN0/edit?usp=sharing">Slack Chatbot Sample</a></h2>
+          <h2 className="m0">
+            <a href="https://docs.google.com/presentation/d/1AGFl4n_bpdYJLvjceJzYUMSJ1dxMYqbqkH1WG69ALN0/edit?usp=sharing">
+              Slack Chatbot Sample
+            </a>
+          </h2>
           <hr />
           <p className="mt0">Demo for my Brooklyn JS Talk about Chatbots</p>
 
-          <h2 className="m0"><a href="https://chrome.google.com/webstore/detail/quote-catalog/ghhlpafpmfocpgoepkhmfbdhhjhjnlfe?hl=en">Quote Catalog for Google Chrome</a></h2>
+          <h2 className="m0">
+            <a href="https://chrome.google.com/webstore/detail/quote-catalog/ghhlpafpmfocpgoepkhmfbdhhjhjnlfe?hl=en">
+              Quote Catalog for Google Chrome
+            </a>
+          </h2>
           <hr />
           <p className="mt0">View a new Quote in every new tab! </p>
 
-          <h2 className="m0"><a href="https://chrome.google.com/webstore/detail/tab-monitor/ndppnnfbbpbcekmmjhkpfnplkppneflk?hl=en">Tab Monitor</a></h2>
+          <h2 className="m0">
+            <a href="https://chrome.google.com/webstore/detail/tab-monitor/ndppnnfbbpbcekmmjhkpfnplkppneflk?hl=en">
+              Tab Monitor
+            </a>
+          </h2>
           <hr />
           <p className="mt0">See how many tabs you open everyday</p>
 
-          <h2 className="m0"><a href="http://cheermeup.io">Cheermeup</a></h2>
+          <h2 className="m0">
+            <a href="http://cheermeup.io">Cheermeup</a>
+          </h2>
           <hr />
-          <p className="mt0">Create a custom meme that you can share with friends.</p>
+          <p className="mt0">
+            Create a custom meme that you can share with friends.
+          </p>
 
-          <h2 className="m0"><a href="http://clintonhalpin.github.io/jade-email-builder/dist/example-email-DDMMYY-version.html">Jade Email Builder</a></h2>
+          <h2 className="m0">
+            <a href="http://clintonhalpin.github.io/jade-email-builder/dist/example-email-DDMMYY-version.html">
+              Jade Email Builder
+            </a>
+          </h2>
           <hr />
           <p className="mt0">Build HTML emails blazing fast with Jade</p>
 
-          <h2 className="m0"><a href="http://clintonhalpin.github.io/is-qa-up/dist/#/">Is QA Up?</a></h2>
+          <h2 className="m0">
+            <a href="http://clintonhalpin.github.io/is-qa-up/dist/#/">
+              Is QA Up?
+            </a>
+          </h2>
           <hr />
           <p className="mt0">Over 100 responses to a simple question.</p>
 
-          <h2 className="m0"><a href="http://kylethrash.com">Kyle Thrash</a></h2>
+          <h2 className="m0">
+            <a href="http://kylethrash.com">Kyle Thrash</a>
+          </h2>
           <hr />
-          <p className="mt0">Website for NYC film director &amp; high-school friend</p>
-          
+          <p className="mt0">
+            Website for NYC film director &amp; high-school friend
+          </p>
         </div>
 
-
         <div className="bg-white">
-
           <div className="container mx-auto mxn2 clearfix sm-py3">
-
-              <h2 className="dark-black p2 m0">Experience With</h2>
-              <div className="px2">
-                <hr />
-              </div>
-
-              <ul className="col col-6 sm-col sm-col-3 list-reset p2">
-                <li className="dark-black bold mb2">Front-end</li>
-                <li>
-                  <ul className="list-reset">
-                    <li className="mb1">Javascript</li>
-                    <li className="mb1">React</li>
-                    <li className="mb1">Angular</li>
-                    <li className="mb1">CSS</li>
-                    <li className="mb1">Build Tools</li>
-                  </ul>
-                </li>
-              </ul>
-
-              <ul className="col col-6 sm-col sm-col-3 list-reset p2">
-                <li className="dark-black bold mb2">Back-end</li>
-                <li>
-                  <ul className="list-reset">
-                    <li className="mb1">Node</li>
-                    <li className="mb1">PHP</li>
-                    <li className="mb1">MYSQL</li>
-                    <li className="mb1">Elasticsearch</li>
-                    <li className="mb1">API Design</li>
-                  </ul>
-                </li>
-              </ul>
-
-              <ul className="col col-6 sm-col sm-col-3 list-reset p2">
-                <li className="dark-black bold mb2">Ops/IT</li>
-                <li>
-                  <ul className="list-reset">
-                    <li className="mb1">AWS Cloud</li>
-                    <li className="mb1">MYSQL</li>
-                    <li className="mb1">Redis</li>
-                    <li className="mb1">Nginx</li>
-                    <li className="mb1">Ansible</li>
-                  </ul>
-                </li>
-              </ul>
-
-              <ul className="col col-6 sm-col sm-col-3 list-reset p2">
-                <li className="dark-black bold mb2">Product Design</li>
-                <li>
-                  <ul className="list-reset">
-                    <li className="mb1">UX</li>
-                    <li className="mb1">UI</li>
-                    <li className="mb1">Agile</li>
-                    <li className="mb1">iOS Design</li>
-                    <li className="mb1">Material Design</li>
-                  </ul>
-                </li>
-              </ul>
+            <h2 className="dark-black p2 m0">Experience With</h2>
+            <div className="px2">
+              <hr />
             </div>
-        </div>        
+
+            <ul className="col col-6 sm-col sm-col-3 list-reset p2">
+              <li className="dark-black bold mb2">Front-end</li>
+              <li>
+                <ul className="list-reset">
+                  <li className="mb1">Javascript</li>
+                  <li className="mb1">React</li>
+                  <li className="mb1">Angular</li>
+                  <li className="mb1">CSS</li>
+                  <li className="mb1">Build Tools</li>
+                </ul>
+              </li>
+            </ul>
+
+            <ul className="col col-6 sm-col sm-col-3 list-reset p2">
+              <li className="dark-black bold mb2">Back-end</li>
+              <li>
+                <ul className="list-reset">
+                  <li className="mb1">Node</li>
+                  <li className="mb1">PHP</li>
+                  <li className="mb1">MYSQL</li>
+                  <li className="mb1">Elasticsearch</li>
+                  <li className="mb1">API Design</li>
+                </ul>
+              </li>
+            </ul>
+
+            <ul className="col col-6 sm-col sm-col-3 list-reset p2">
+              <li className="dark-black bold mb2">Ops/IT</li>
+              <li>
+                <ul className="list-reset">
+                  <li className="mb1">AWS Cloud</li>
+                  <li className="mb1">MYSQL</li>
+                  <li className="mb1">Redis</li>
+                  <li className="mb1">Nginx</li>
+                  <li className="mb1">Ansible</li>
+                </ul>
+              </li>
+            </ul>
+
+            <ul className="col col-6 sm-col sm-col-3 list-reset p2">
+              <li className="dark-black bold mb2">Product Design</li>
+              <li>
+                <ul className="list-reset">
+                  <li className="mb1">UX</li>
+                  <li className="mb1">UI</li>
+                  <li className="mb1">Agile</li>
+                  <li className="mb1">iOS Design</li>
+                  <li className="mb1">Material Design</li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
 
         <div className="bg-gray mt4">
           <div className="px2">
             <div className="table col-12 p2">
               <div className="table-cell center">
-                <a className="bold black inline-block no-underline" href="/">&copy; 2017 Clinton Halpin</a><br />
+                <a className="bold black inline-block no-underline" href="/">
+                  &copy; 2017 Clinton Halpin
+                </a>
+                <br />
                 <span className="gray">clintonhalpin(at)gmail.com</span>
               </div>
             </div>
           </div>
-        </div>        
-
+        </div>
       </div>
     );
   }
